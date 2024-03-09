c0f011c190ace0bf05f27ef36ee4d4e0284ff213
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-08 21:00:37 -0600

    Create Employee Entity

---
+++ resources/GetDemo/Core/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public int ManagerId { get; set; }
+        public Employee Manager { get; set; }
+    }
+}

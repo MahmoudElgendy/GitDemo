3e92b8507a8cb1ec0c25f9516754ab7b7032de40
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-08 21:03:51 -0600

    Create EmployeeContext

---
+++ resources/GetDemo/Infrastructure/Data/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
